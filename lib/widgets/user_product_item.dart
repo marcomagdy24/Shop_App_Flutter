63261da28de6fcae8eb218031676f80b4e56a7ab
Marco Magdy <marcomagdy241@gmail.com>
2021-03-16 01:46:11 +0200

    add user product item widget

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../screens/edit_product_screen.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  const UserProductItem(this.title, this.imageUrl);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(title),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(imageUrl),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () =>
+                  Navigator.of(context).pushNamed(EditProductScreen.routeName),
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {},
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
