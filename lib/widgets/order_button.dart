639a2ca32944d783fefc570e97aae585169c72fd
Marco Magdy <marcomagdy241@gmail.com>
2021-03-17 04:20:44 +0200

    custom order button

---
+++ lib/widgets/order_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
+import '../providers/orders.dart';
+import '../screens/orders_screen.dart';
+import './progress_indicator.dart';
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  _OrderButtonState createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: _isLoading ? CustomProgressIndicator() : Text("ORDER NOW"),
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+          ? null
+          : () async {
+              setState(() {
+                _isLoading = true;
+              });
+              await Provider.of<Orders>(context, listen: false).addOrder(
+                widget.cart.items.values.toList(),
+                widget.cart.totalAmount,
+              );
+              setState(() {
+                _isLoading = true;
+              });
+              widget.cart.clearCart();
+            },
+    );
+  }
+}
