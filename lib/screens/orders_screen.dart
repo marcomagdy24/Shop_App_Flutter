7d971b20047dc697540a3fa2e39ec238af679f18
Marco Magdy <marcomagdy241@gmail.com>
2021-03-17 04:22:37 +0200

    extract orderItem widget

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,23 +4,42 @@ import 'package:provider/provider.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
+import '../widgets/progress_indicator.dart';
 
 class OrderScreen extends StatelessWidget {
   static const routeName = "/orders";
+
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      drawer: AppDrawaer(),
-      appBar: AppBar(
-        title: Text("Your Orders"),
-      ),
-      body: ListView.builder(
-        itemBuilder: (_, index) {
-          return OrderItem(orderData.orders[index]);
-        },
-        itemCount: orderData.orders.length,
-      ),
-    );
+        drawer: AppDrawaer(),
+        appBar: AppBar(
+          title: Text("Your Orders"),
+        ),
+        // ignore: missing_return
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CustomProgressIndicator();
+            } else {
+              if (snapshot.error != null) {
+                return Center(
+                  child: Text('Error'),
+                );
+              } else {
+                return Consumer<Orders>(
+                  builder: (ctx, orderData, child) => ListView.builder(
+                    itemBuilder: (ctx, index) {
+                      return OrderItem(orderData.orders[index]);
+                    },
+                    itemCount: orderData.orders.length,
+                  ),
+                );
+              }
+            }
+          },
+        ));
   }
 }
