58648491bf759dbcc552aa5b9755332fd62a76f7
Marco Magdy <marcomagdy241@gmail.com>
2021-03-17 04:29:55 +0200

    add Products Provider

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
 import '../screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
-  const UserProductItem(this.title, this.imageUrl);
+  const UserProductItem(this.id, this.title, this.imageUrl);
   @override
   Widget build(BuildContext context) {
+    final scaffold = ScaffoldMessenger.of(context);
     return Card(
       child: ListTile(
         title: Text(title),
@@ -20,13 +24,26 @@ class UserProductItem extends StatelessWidget {
           children: [
             IconButton(
               icon: Icon(Icons.edit),
-              onPressed: () =>
-                  Navigator.of(context).pushNamed(EditProductScreen.routeName),
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
+              },
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
               icon: Icon(Icons.delete),
-              onPressed: () {},
+              onPressed: () async {
+                try {
+                  await Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (error) {
+                  scaffold.showSnackBar(
+                    SnackBar(
+                      content: Text("Deleting Failed!"),
+                    ),
+                  );
+                }
+              },
               color: Theme.of(context).errorColor,
             ),
           ],
