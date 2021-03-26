0f4d8849ebe706795d9af6cc78ca4773addd5dbe
Marco Magdy <marcomagdy241@gmail.com>
2021-03-26 02:54:49 +0200

    make it stateful widget

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+import '../widgets/progress_indicator.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
   static const routeName = "/product-detail";
 
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
     final String productId = ModalRoute.of(context).settings.arguments;
@@ -13,43 +19,82 @@ class ProductDetailScreen extends StatelessWidget {
       context,
       listen: false,
     ).findById(productId);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(item.title),
-      ),
-      body: Column(children: [
-        Container(
-          height: ((MediaQuery.of(context).size.height -
-                  MediaQuery.of(context).padding.horizontal) /
-              2),
-          width: double.infinity,
-          padding: EdgeInsets.all(10),
-          child: Card(
-            child: Image.network(
-              item.imageUrl,
-              fit: BoxFit.cover,
+      // appBar: AppBar(
+      //   title: Text(item.title),
+      // ),
+
+      body: CustomScrollView(
+        primary: true,
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            iconTheme: IconThemeData(color: Colors.black),
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              title: AnimatedOpacity(
+                duration: Duration(milliseconds: 300),
+                opacity: 1,
+                child: Text(
+                  item.title,
+                ),
+              ),
+              background: Hero(
+                tag: item.id,
+                child: Card(
+                  child: Image.network(
+                    item.imageUrl,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) =>
+                        CustomProgressIndicator(),
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 10),
-        Text(
-          "\$${item.price}",
-          style: TextStyle(
-            color: Colors.grey,
-            fontSize: 20,
-          ),
-        ),
-        SizedBox(height: 10),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          child: Text(
-            item.description,
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18, color: Colors.grey[600]),
-            softWrap: true,
-          ),
-        ),
-      ]),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(height: 10),
+              Text(
+                "\$${item.price}",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  item.description,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+                  softWrap: true,
+                ),
+              ),
+              SizedBox(
+                height: 800,
+              )
+            ]),
+          )
+        ],
+      ),
     );
   }
 }
+
+
+// Column(children: [
+//                 Container(
+//                   height: ((MediaQuery.of(context).size.height -
+//                           MediaQuery.of(context).padding.horizontal) /
+//                       2),
+//                   width: double.infinity,
+// //                   padding: EdgeInsets.all(10),
+//                   child: 
+//                 ),
+                
+//               ]),
