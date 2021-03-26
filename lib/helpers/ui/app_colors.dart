5f5d932bb0c8b5551a3c46a77aa05dbf7871ab97
Marco Magdy <marcomagdy241@gmail.com>
2021-03-26 02:41:06 +0200

    add app colors

---
+++ lib/helpers/ui/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const Color primarySwatch = Colors.grey;
+const Color primaryColor = Color(0xFF5D1049);
+const Color primaryLightColor = Color(0xFF720D5D);
+const Color primaryDarkColor = Color(0xFF4E0D3A);
+const Color backgroundColor = Color(0xFFE5E5E5);
+const Color accentColor = Color(0xFFE30425);
+const Color textColor = Color(0xFF442C2E);
+const Color iconThemeColor = Colors.white;
+const Color dividerColor = Colors.white54;
+const Color commentColor = Color.fromARGB(255, 255, 246, 196);
+
+// primarySwatch: ,
+//   primaryColor: ,
+//   brightness: Brightness.light,
+//   backgroundColor: ,
+//   accentColor: ,
+//   accentIconTheme: IconThemeData(color: Colors.white),
+//   dividerColor: ,
+// const Color primaryColor = Color(0xFF5D1049);
+// const Color accentColor = Color(0xFFE30425);
+// const Color textColor = Color(0xFF6D6D6D);
+// const Color backgroundColor = Color(0xFFF3F3F3);
+// const Color commentColor = Color.fromARGB(255, 255, 246, 196);
