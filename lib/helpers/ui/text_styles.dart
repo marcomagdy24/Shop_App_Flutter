5566d76949555352107edfbaacf7106241853cdc
Marco Magdy <marcomagdy241@gmail.com>
2021-03-26 02:41:19 +0200

    add text styles

---
+++ lib/helpers/ui/text_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import './app_colors.dart';
+
+const headline4 =
+    TextStyle(fontSize: 19, fontWeight: FontWeight.w700, color: accentColor);
+const subtitle1 = TextStyle(fontSize: 14.0, color: Colors.black);
+const button =
+    TextStyle(fontSize: 14.0, fontWeight: FontWeight.w900, color: Colors.white);
+const subtitle2 = TextStyle(fontSize: 17.0, color: Color(0xFFC6C3C3));
+const headline5 =
+    TextStyle(fontSize: 18.0, fontWeight: FontWeight.w700, color: textColor);
+const headline6 =
+    TextStyle(fontSize: 16, fontWeight: FontWeight.w300, color: textColor);
+const caption = TextStyle(fontSize: 12, color: textColor);
+const headline3 =
+    TextStyle(fontSize: 34, fontWeight: FontWeight.w300, color: textColor);
